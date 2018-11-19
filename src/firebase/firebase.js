2f65da823197a590684315625d224fb3aa3650d7
Kevin Tsai <tsai.kevin.j@gmail.com>
2018-11-19 11:58:35 -0800

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyBINchPPVEHpp0WaC_kTxBh90hxkkbWQRg",
-    authDomain: "expensify-6c8ad.firebaseapp.com",
-    databaseURL: "https://expensify-6c8ad.firebaseio.com",
-    projectId: "expensify-6c8ad",
-    storageBucket: "expensify-6c8ad.appspot.com",
-    messagingSenderId: "114007408359",
+  apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSENGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
