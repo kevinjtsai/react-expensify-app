a58ff419a3401afa02a6d3d3eab7a407399981e8
Kevin Tsai <tsai.kevin.j@gmail.com>
2018-12-09 14:56:01 -0500

    heroku firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,11 +2,11 @@ import * as firebase from 'firebase';
 
 var config = {
   apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSENGING_SENDER_ID,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSENGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
