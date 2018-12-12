4524f93f4f5d2771fe94edbeb3ea1256539ab38d
Kevin Tsai <tsai.kevin.j@gmail.com>
2018-12-12 11:31:09 -0500

    fixes edit expense button text

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -89,7 +89,7 @@ export default class ExpenseForm extends React.Component {
           onChange={this.onNoteChange}
         />
         <div>
-          <button className="button">Add Expense</button>
+          <button className="button">{this.props.expense ? 'Edit Expense' : 'Add Expense'}</button>
         </div>
       </form>
     );
